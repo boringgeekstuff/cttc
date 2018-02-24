9caeac9325d4d406a2f9983e0f7749afe411e5ed
roshek <roshek34@mail.ru>
2018-02-24 23:02:14 +0000

    Basic implementation

---
+++ index.js
@@ -0,0 +1,77 @@
+log=console.log;
+
+var PORT = process.env.PORT || 8080;
+
+var express = require('express');
+var WebSocketServer = require("ws").Server;
+var http = require("http");
+var url = require('url');
+
+var app = express();
+
+app.use('/res',express.static('res'));
+
+app.get('/room/:roomId',(req,res)=>{
+    res.sendFile(__dirname + '/room.html');
+});
+
+var httpServer = http.createServer(app);
+
+server = new WebSocketServer({ 
+    noServer:true
+});
+
+var rooms = {};
+
+httpServer.on('upgrade', (request, socket, head) => {
+    var pathname = url.parse(request.url).pathname;
+
+    var match = pathname.match(/\/room\/([0-9]+)/);
+    var roomId = match[1];
+    if(match && rooms[roomId]!=true){
+        server.handleUpgrade(request, socket, head, (ws) => {
+            if(rooms[roomId]===true){
+                ws.close();
+            }else if(rooms[roomId]){
+                var perf = createPerf();
+                var user = rooms[roomId];
+                user.on('message',(m)=>{
+                    ws.send(m);
+                });
+                ws.on('message',(m)=>{
+                    user.send(m);
+                });
+                user.on('close',()=>{
+                    ws.close();
+                });
+                ws.on('close',()=>{
+                    user.close();                    
+                });
+                rooms[roomId] = true;
+            }else{
+                rooms[roomId] = ws;
+                ws.on('close',()=>{
+                    delete rooms[roomId];
+                });
+            }
+        });
+    }else{
+        socket.destroy();
+    }
+});
+
+function createPerf(){
+    var time = process.hrtime();
+    var itemsProcessed = 0;
+    return (items)=>{
+        itemsProcessed+=items;
+        var elapsed = process.hrtime(time);
+        if(elapsed[0]>0){
+            log('IPS ' + (itemsProcessed/(elapsed[0]+elapsed[1]/1000000000)));
+            time = process.hrtime();
+            itemsProcessed=0;
+        }
+    };
+}
+
+httpServer.listen(PORT,()=>log('App up on http://localhost:' + PORT));
---
+++ res/js/util.js
@@ -0,0 +1,112 @@
+log=console.log;
+
+Args = {
+	swap : (fn)=>(a,b,...rest)=>fn(b,a,...rest)
+};
+
+
+Array.prototype.callEach = function(self,...args){
+    this.applyEach(self,args);
+};
+
+Array.prototype.applyEach = function(self,args){
+    for(var i=0;i<this.length;i++){
+        this[i].apply(self,args);
+    }
+    return this;
+};
+
+function createExpandingObject(factory,o={}){
+    return new Proxy(o,{
+        get:(t,p)=>(t[p]||(t[p]=factory(p)))
+    });
+}
+
+
+function EventEmitter(){
+    var ee = this;
+    var handlers = createExpandingObject(_=>[]);
+    ee.on = function(event,handler){
+        handlers[event].push(handler);
+        return ee;
+    };
+    ee.off = function(event,handler){
+        var index = handlers[event].indexOf(handler);
+        if(index!==-1){
+            handlers[event].splice(index,1);
+        }
+        return ee;
+    };
+    ee.once = function(event,handler){
+        handler = (h=>()=>{
+            ee.off(event,handler);
+            h();
+        })(handler);
+        ee.on(event,handler);
+        return ee;
+    };
+    ee.emit = function(event,payload){
+        handlers[event].callEach(ee,payload,event);
+        return ee;
+    };
+}
+
+var EE = EventEmitter;
+
+EE.onEvery = function(event,fn,ees){
+	ees.forEach(e=>e.on(event,fn));
+};
+
+EE.onceEvery = function(event,fn,ees){
+	ees.forEach(e=>e.once(event,fn));
+};
+
+EE.emitEvery = function(event,payload,ees){
+	ees.forEach(e=>e.emit(event,payload));
+};
+
+EE.offEvery = function(event,fn,ees){
+	ees.forEach(e=>e.off(event,fn));
+};
+
+EE.broadcast = function(event,ees){
+	function broadcast(data){
+		EE.offEvery(event,broadcast,ees);
+		EE.emitEvery(event,data,ees);
+	}
+	EE.onceEvery(event,broadcast,ees);
+};
+
+EventEmitter.prototype.emitter = function(event){
+	return (payload)=>this.emit(event,payload);
+};
+
+function Intermitter(ee,interceptors){
+    EventEmitter.apply(this);
+    Object.entries(interceptors).forEach(([event,fn])=>{
+        this.on(event,(data)=>fn(data)?ee.emit(event,data):'');
+    });
+}
+
+Intermitter.prototype=EventEmitter.prototype;
+
+var Pipe = {
+	syncFn : function(fn){
+		return (d,cb)=>cb(fn(d));
+	},
+	delayFirst : function(time){
+		var first = true;
+		return (d,cb)=>first?(first=(setTimeout(()=>cb(d),time) && false)):cb(d);
+	}
+};
+
+
+Promise.delay = (delay)=>new Promise(r=>setTimeout(r, delay));
+
+Promise.callDelay = (delay,fn)=>Promise.delay(delay).then(fn);
+
+function float32Concat(iterable){
+	var result = new Float32Array(iterable.reduce((a,b)=>a+b.length,0));
+	iterable.reduce((o,a)=>{result.set(a,o);return o+a.length},0);
+	return result;
+}
